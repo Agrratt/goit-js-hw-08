48d349cb5d8312116448b7f20133c467aaf67cf8
Kateryna Tolstykh <agrratt@gmail.com>
2021-12-26 19:56:01 +0200

    add exercise-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const listContainer = document.querySelector('.gallery');
+
+const createPhotoCard = galleryItems.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+            </a>
+        `;
+
+    }).join('');
+
+listContainer.insertAdjacentHTML('beforeend', createPhotoCard);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+});
 
 console.log(galleryItems);
