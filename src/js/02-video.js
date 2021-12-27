a43574f1aa5818d5f829e80dd2a1c426f3cbd5a6
Kateryna Tolstykh <agrratt@gmail.com>
2021-12-27 18:17:21 +0200

    Add exercise-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const CUREENT_TIME = 'current-time'
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.on('timeupdate',
+  throttle(() => {
+  player.getCurrentTime().then(sec => {
+    localStorage.setItem(CUREENT_TIME, JSON.stringify(sec));
+    console.log(sec)
+  });
+}, 1000),
+);
+
+const videoStart = JSON.parse(localStorage.getItem(CUREENT_TIME));
+player.setCurrentTime(videoStart);
